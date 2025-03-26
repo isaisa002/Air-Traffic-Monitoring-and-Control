3e08e9b1245b47d841bc9dd7bdc4a544fe945341
Pepen0 <penoelothibeaud1@gmail.com>
2025-03-26 19:58:24 -0400

    Removed build stuff

--- ATC_Shared_Files/build/ATC_Shared_Files/src/Timer.d
+++
@@ -1,3 +0,0 @@
-build/x86_64-debug/../ATC_Shared_Files/src/Timer.o: \
- ../ATC_Shared_Files/src/Timer.cpp \
- c:\users\isach\downloads\atc_system\atc_shared_files\public\timer.h
--- Aircraft_Manager/build/x86_64-debug/src/Aircraft_Manage.d
+++
@@ -1,3 +0,0 @@
-build/x86_64-debug/src/Aircraft_Manage.o: src/Aircraft_Manage.cpp \
- src/Aircraft_Manager.h ../ATC_Shared_Files/public/Shared_Memory.h \
- ../ATC_Shared_Files/public/Logger.h ../ATC_Shared_Files/public/Timer.h
